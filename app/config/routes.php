6fc8247df70038a02a48f299587b9d760b0fc04c
Mary Rose Pangilinan <pangilinanmary11@gmail.com>
2026-09-10 10:54:46 +0800

    Set login as homepage

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,7 +44,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+$router->get('/', 'LoginController::index');
 
 $router->get('/products', 'ProductsController::index');
 
